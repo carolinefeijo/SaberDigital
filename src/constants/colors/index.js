1541c53be96c2f6e6226c4a0ac2db67d0978357b
caroline <contatocarolinefeijo@gmail.com>
2022-06-28 17:27:55 -0300

    school_app

---
+++ src/constants/colors/index.js
@@ -0,0 +1,21 @@
+export const primary = {
+    color10: '#6B6B6B',
+    color20: '#F3B2B2',
+    color30: '#6C6C6C'
+}
+export const blue = {
+    color10: '#2DA7FF',
+}
+export const red = {
+    color10: '#EA1B1B'
+}
+export const white = {
+    color0: '#FFFFFF'
+}
+export const neutral = {
+    color10: '#4B4A4A',
+   
+}
+export const black = {
+    color10: '#000000',
+}
---
+++ src/constants/fontsSize/index.js
@@ -0,0 +1,4 @@
+export const textMedium = 18
+export const fontWeightSmall = '500'
+export const fontWeightMedium = '600'
+export const fontWeightLarger = '800'
